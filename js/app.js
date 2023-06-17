c2b9b42d2592ce82e08d773c26df1727a985d7b8
Ariania Palmer <arianiapalmer@gmail.com>
2023-06-17 19:50:42 -0400

    building game logic

--- js/app.js
+++ js/app.js
@@ -1,1 +1,68 @@
 'use strict';
+let allCards = [];
+let cardNames = ['backside', 'boxcat', 'dishwasher', 'fetcher', 'licker', 'monorail', 'puppyeye', 'snuggler'];
+// let pair1 = [];
+// let pair2 = [];
+let dataList =[...cardNames, ...cardNames];
+let cardCount = dataList.length;
+let cardsDisplay = document.getElementById('cards-display');
+let activeTile = null;
+let awaitingEndOfMove = false;
+
+// Should player go in a seperate player.js file?
+// let player = {
+//     // playerName: ,
+//     playerScore: 0,
+// }
+
+// Construction function for creating card objects
+function Card (name, pairStack){
+  this.name = name;
+  this.image = `img/${name}.png`;
+  this.pairStack = pairStack;
+  this.push(allCards);
+}
+// Usingt the construct funtion to create cards for each cardname index
+function createCards(){
+  for(let i=0; i<cardNames.length; i++){
+    new Card(cardNames[i], '1');
+  }
+  for(let i=0; i<cardNames.length; i++){
+    new Card(cardNames[i], '2');
+  }
+  console.log(allCards);
+}
+//Create card images and setting attributes
+function setGame(card){
+  let cardImage = document.createElement('img');
+  cardImage.classList.add('card');
+  cardImage.setAttribute('src', card.image);
+  cardImage.setAttribute('data-revealed', false);
+  cardImage.addEventListener('click', handleFlip);
+  resetCard();
+
+}
+function resetCard(){
+
+}
+
+function handleFlip(event){
+  const revealed = event.target.getAttribute('data-revealed');
+//   let activeTile = null;
+//   let awaitingEndOfMove = false;
+    if(awaitingEndOfMove || re)
+}
+//Build Cards
+function buildCards(){
+  for (let i=0; i<cardCount; i++){
+    let randomIndex = Math.floor(Math.random()*dataList);
+    let image = dataList[i][randomIndex];
+    let card = setGame(image);
+    dataList[i].splice(image);
+    cardsDisplay.appendChild(card);
+  }
+}
+
+//create audio functions
+
+createCards();
