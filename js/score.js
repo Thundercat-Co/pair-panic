f2a7f53387cf9cbe5603d0c703a6a6c51e006c75
Ariania Palmer <arianiapalmer@gmail.com>
2023-06-21 08:05:28 -0400

    added score.js

---
+++ js/score.js
@@ -0,0 +1,5 @@
+'use strict';
+// send all player array back to local storage with a new key
+// get data from local storage
+// use a loop to append to scores.html
+// possibly display in order of who did the best using .sort() array method?
